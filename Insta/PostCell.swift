bff640d70d031e2e83d9f29498497eff7bade639
julianaelrayes <elrayes@usf.edu>
2022-03-01 14:44:41 -0500

    new project

---
+++ Insta/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Insta
+//
+//  Created by Juliana El Rayes on 2/26/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
